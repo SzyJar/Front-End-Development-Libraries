8359637bf510a5c1c93832d0ab058c52474fb7fb
SzyJar <107247457+SzyJar@users.noreply.github.com>
2023-04-10 03:47:37 +0200

    Markdown Previewer - done

---
+++ Markdown Previewer/script.js
@@ -0,0 +1,66 @@
+import React from "https://cdn.skypack.dev/react@17.0.1";
+import ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
+import { useState } from "https://cdn.skypack.dev/react@17.0.1";
+import "https://cdnjs.cloudflare.com/ajax/libs/marked/2.0.3/marked.min.js";
+
+function ReactApp() {
+  const [markdown, setMarkdown] = useState(`# Markdown Previewer
+
+## Introduction
+
+This is a simple Markdown previewer built with React. It allows you to enter Markdown text in the editor and see the rendered HTML in real-time in the preview panel.
+
+### Features
+
+- Syntax highlighting for code blocks
+- Automatic line breaks for paragraphs
+- Support for all standard Markdown syntax
+
+#### Code Example
+
+Here's an example of a code block in JavaScript:
+
+\`\`\`javascript
+function helloWorld() {
+  console.log("Hello, world!");
+}
+\`\`\`
+
+Here's another example of code in JavaScript.
+\`const message = 'Hello, world!';\`
+                                            
+#### Link Example
+
+Here's a link to [Google](https://www.google.com).
+
+#### Image Example
+
+Here's an image of a cute kittens:
+
+![Kittens](https://www.petmd.com/sites/default/files/petmd-kitten-facts.jpg)
+
+#### Bold Example
+
+This text is **bold**.
+
+#### Blockquote Example
+
+> The best way to predict the future is to create it.`);
+
+  function handleChange(event) {
+    setMarkdown(event.target.value);
+  }
+  const renderMarkdown = () => {
+    const html = marked(markdown, { breaks: true });
+    return { __html: html };
+  };
+
+  return /*#__PURE__*/(
+    React.createElement("div", null, /*#__PURE__*/
+    React.createElement("textarea", { id: "editor", onChange: handleChange, value: markdown }), /*#__PURE__*/
+    React.createElement("div", { className: "Preview", id: "preview", dangerouslySetInnerHTML: renderMarkdown() })));
+
+
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(ReactApp, null), document.getElementById("root"));
